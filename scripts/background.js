da2333df43d34524400d0f0de3ea3e0060160785
galbenitzhak <galben2002@gmail.com>
2023-09-29 10:52:05 -0700

    Edits

--- scripts/background.js
+++ scripts/background.js
@@ -3,14 +3,16 @@ window.addEventListener('load', function(){
     for (var i = 0; i < expand_btns.length; i++){
         expand_btns[i].addEventListener("click", function(e){
             target_name = e.currentTarget.id.split('_')[0];
+            plus_btn = target_name + "_plus";
             target_name = target_name + "_div";
             target = document.getElementById(target_name);
+            plus = document.getElementById(plus_btn);
             if(target.classList.contains("hidden")){
-                e.currentTarget.innerHTML = "-";
+                plus.innerHTML = "-";
                 target.classList.remove("hidden");
                 target.classList.add("shown");
             }else{
-                e.currentTarget.innerHTML = "+";
+                plus.innerHTML = "+";
                 target.classList.remove("shown");
                 target.classList.add("hidden");
             }
@@ -23,4 +25,32 @@ window.addEventListener('load', function(){
             target.scrollIntoView({behavior: 'smooth'});
         });
     }
+    
+    var modal = this.document.getElementById("popupModal");
+
+    var imgs = document.getElementsByClassName("supporting");
+    var modalImg = document.getElementById("modalImg");
+    var captionText = document.getElementById("caption");
+    for(var i = 0; i < imgs.length; i++){
+        imgs[i].onclick = function(){
+            modal.style.display = "block";
+            modalImg.src = this.src;
+            captionText.innerHTML = this.alt;
+            modalImg.alt = this.alt;
+        }
+    }
+
+    var closeBtn = document.getElementsByClassName("close")[0];
+    closeBtn.onclick = function(){
+        modal.style.display = "none";
+    }
+
 });
+
+function mailrep(e){
+    j = e.replaceAll('/', '');
+    j = j.replaceAll('-', '');
+    s = j.replaceAll('.', '');
+    var a = atob(s);
+    window.location.href = "mailto:" + a;
+}
